6a10c289ffeca4abc63c169b94cb4580fc08b6fe
AcchonBurike <AcchonBurike@example.com>
2025-07-01 17:57:05 +0900

    リスト内の各タスクに、タスク名・「完了」ボタン・削除ボタンが含まれるように更新

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const listDiv = document.getElementById('list-div');
 
 const tasks = [];
 
+// li要素用テンプレート
+const listHTMLstr = `
+  <li>
+      <div class="task-header">
+        <input id="task-complete-button" type="checkbox">
+        <span id="list-name"></span>
+      </div>
+      <button id="task-delete-button" type="button">タスクを削除する</button>
+  </li>
+`;
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const newTask = document.getElementById('task-input').value;
@@ -21,19 +32,28 @@ form.addEventListener('submit', (event) => {
   console.log(tasks); // デバッグ用
 })
 
+
+// HTML文字列をElementオブジェクトに変換する関数
+function htmlStrToElement(listHTMLstr) {
+  const dummyDiv = document.createElement('div');
+  dummyDiv.innerHTML = listHTMLstr;
+  return dummyDiv.firstElementChild;
+};
+
 // リスト変更時にリストを再描写する関数
 function refreshList() {
   // 既存のリストを初期化
   document.querySelectorAll('#list-div ul').forEach(el => el.remove());
 
-  // ul要素を作成して挿入
-  const ulElement = document.createElement('ul');
-  listDiv.append(ulElement);
+  // ul要素を作成
+  const newElement = htmlStrToElement(listHTMLstr);
 
-  // li要素を作成して挿入
+  // 配列の数だけリスト要素を作成して挿入
   for (let task of tasks) {
-    const newList = document.createElement('li');
-    newList.textContent = task;
-    document.querySelector('#list-div ul').append(newList);
+    // 新リスト要素のタスク名テキストを代入
+    newElement.querySelector('#list-name').textContent = task;
+    // リストdivにli要素を追加
+    listDiv.append(newElement);
+    // document.querySelector('#list-div ul').append(newList);
   }
 };

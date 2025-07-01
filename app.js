6f645f592140814240ebd57404fd8e3428a23a09
AcchonBurike <AcchonBurike@example.com>
2025-07-01 16:35:51 +0900

    追加したタスクがリスト表示されるように更新

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 'use strict';
 
 const form = document.getElementById('form-input');
+const listDiv = document.getElementById('list-div');
 
 const tasks = [];
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const newTask = document.getElementById('task-input').value;
-  console.log(newTask);
-  tasks.push(newTask);
-  console.log(tasks);
-  document.getElementById('task-input').value = '';
+  console.log(newTask); // デバッグ用
+
+  // タスク名が空でなければ、tasks配列に追加
+  if (newTask !== '') {
+    tasks.push(newTask);
+    document.getElementById('task-input').value = '';
+
+    // HTMLの更新
+    refreshList();
+  }
+  console.log(tasks); // デバッグ用
 })
+
+function refreshList() {
+  // 既存のリストを初期化
+  document.querySelectorAll('#list-div ul').forEach(el => el.remove());
+
+  // ul要素を作成して挿入
+  const ulElement = document.createElement('ul');
+  listDiv.append(ulElement);
+
+  // li要素を作成して挿入
+  for (let task of tasks) {
+    const newList = document.createElement('li');
+    newList.textContent = task;
+    document.querySelector('#list-div ul').append(newList);
+  }
+};

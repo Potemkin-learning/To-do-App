bc949e0f11e7d60cfb46a145db43fa8db420db98
AcchonBurike <AcchonBurike@example.com>
2025-07-01 13:46:55 +0900

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const form = document.getElementById('form-input');
+
+const tasks = [];
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const newTask = document.getElementById('task-input').value;
+  console.log(newTask);
+  tasks.push(newTask);
+  console.log(tasks);
+})
